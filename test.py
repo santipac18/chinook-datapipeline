eb56c499c2ae660cf6a0fb24a4fc197be304fe6c
santipac18 <santipac18>
2024-10-30 07:06:55 +0000

    test case เช็คมากกว่า 2

--- test.py
+++ test.py
@@ -1,38 +1,5 @@
 # Databricks notebook source
 import pandas as pd
-from datetime import datetime
-
-# file path
-inputPath = '/Workspace/Users/santipac@ais.co.th/track_small.csv'
-outputPath = '/Workspace/Users/santipac@ais.co.th/output_small.csv'
-testResultPath = '/Workspace/Users/santipac@ais.co.th/test_result.txt'
-
-# open test result file
-f = open(testResultPath, "w")
-
-# read csv file
-tracksInput = pd.read_csv(inputPath)
-tracksOutput = pd.read_csv(outputPath)
-
-# Case1 1
-if tracksOutput.dtypes["UnitPrice"] == 'int64':
-    f.write("Case 1: Passed\n")
-else:
-    f.write("Case 1: Failed\n")
-
-# Case 2
-mergedTracks = tracksInput.merge(tracksOutput, on="TrackId", suffixes=("_input", "_output"))
-if (mergedTracks['UnitPrice_output'] - mergedTracks['UnitPrice_input'] < 1).all():
-    f.write("Case 2: Passed\n")
-else:
-    f.write("Case 2: Failed\n")
-
-# close test result file
-f.close()
-
-# COMMAND ----------
-
-import pandas as pd
 import configparser
 import os
 from datetime import datetime
@@ -68,7 +35,7 @@ else:
 
 # Case 2
 mergedTracks = pd.merge(tracksInput, tracksOutput, on='TrackId', suffixes=('_input', '_output'))
-if (mergedTracks['UnitPrice_output'] - mergedTracks['UnitPrice_input'] < 1).all():
+if (mergedTracks['UnitPrice_output'] - mergedTracks['UnitPrice_input'] < 2).all():
     f.write("Case 2: Pass\n")
 else:
     f.write("Case 2: Fail\n")
